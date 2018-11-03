0642e397ba70f64e7aa4a5141346cc6cee3745bc
David Fulop <28536613+davidfulop@users.noreply.github.com>
2018-11-03 22:34:40 +0000

    Add DefaultValueInInterfaces example code

---
+++ DefaultValueInInterfaces/src/DefaultValueInInterfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace DefaultValueInInterfaces
+{
+    class Program
+    {
+        static void Main()
+        {
+            IStringReverser reverser = GetStringReverser();
+            string reversed = reverser.Reverse();
+            Console.WriteLine(reversed);
+        }
+
+        private static IStringReverser GetStringReverser()
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            Type tsr = asm.GetType("DefaultValueInInterfaces.StringReverser");
+            return Activator.CreateInstance(tsr) as IStringReverser;
+        }
+    }
+
+    public interface IStringReverser
+    {
+        string Reverse(string s = "Picard");
+    }
+
+    public class StringReverser : IStringReverser
+    {
+        public string Reverse(string s = "Archer")
+        {
+            return new string(s.Reverse().ToArray());
+        }
+    }
+}
